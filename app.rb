1f60472969b8377f16987f18df87dcecb5b23994
A.S. Lomoff <lomov.as@gmail.com>
2015-08-14 18:04:00 +0300

    add fibonacci

--- app.rb
+++ app.rb
@@ -5,3 +5,8 @@ get '/' do
   cpu_name = `uname -m`.strip
   erb :index, locals: {cpu: cpu_name}
 end
+
+get '/fibonacci/:number' do |number|
+  value = (0..number.to_i).inject([1,0]) { |(a,b), _| [b, a+b] }[0]
+  erb :number, locals: {number: number, value: value}
+end
