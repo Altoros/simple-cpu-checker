a6e8df7f093a76cb91e07588040e552ae4ddb7ae
A.S. Lomoff <lomov.as@gmail.com>
2015-10-21 13:47:09 +0300

    some changes to see more information about cpu

--- app.rb
+++ app.rb
@@ -10,3 +10,9 @@ get '/fibonacci/:number' do |number|
   value = (0..number.to_i).inject([1,0]) { |(a,b), _| [b, a+b] }[0]
   erb :number, locals: {number: number, value: value}
 end
+
+get '/lscpu' do
+  output = `lscpu`
+  output = $?.success? ? output.gsub!(/\n/, '<br/>') : 'Command is not supported'
+  erb :lscpu, locals: {output: output}
+end
